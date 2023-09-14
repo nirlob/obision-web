d6293b4b6720dcbd4ab600319964e137a227c39f
Jose Francisco Gonzalez <jfgs1609@gmail.com>
2023-09-14 23:13:44 +0200

    Commit

--- src/main/java/com/obision/web/controllers/DownloadController.java
+++ src/main/java/com/obision/web/controllers/DownloadController.java
@@ -3,12 +3,6 @@ package com.obision.web.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.apachecommons.CommonsLog;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,17 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.obision.web.models.Download;
 import com.obision.web.repositories.DownloadsRepository;
 
-import java.io.File;
-import java.io.FileInputStream;
-
 
 @RestController
 @RequestMapping("/downloads")
 @CommonsLog
 public class DownloadController {
-    @Value("${downloads.path}")
-    private String downloadsPath;
-    
     private final DownloadsRepository downloadsRepository;
 
     public DownloadController(DownloadsRepository downloadsRepository) {
@@ -36,7 +24,7 @@ public class DownloadController {
     }
 
     @GetMapping("/{version}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String version, HttpServletRequest request) throws Exception {
+    public ResponseEntity<String> downloadFile(@PathVariable String version, HttpServletRequest request) throws Exception {
         Download download = new Download();
 
         try {
@@ -48,17 +36,6 @@ public class DownloadController {
             log.warn("Can't save download to database", e);
         }
 
-        File downloadFile = new File(downloadsPath + "/obision-" + version + ".iso");
-        // InputStreamResource resource = new InputStreamResource(new FileInputStream(downloadFile));
-        FileSystemResource resource = new FileSystemResource(downloadFile); 
-
-        HttpHeaders header = new HttpHeaders();
-        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + downloadFile.getName());
-
-        return ResponseEntity.ok()
-                .headers(header)
-                .contentLength(downloadFile.length())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
-                .body(resource);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/resources/static/js/downloads.js
+++ src/main/resources/static/js/downloads.js
@@ -1,22 +1,10 @@
-function download(version, sender) {
-    const element = document.getElementById(sender);
-    const innerHtml = element.innerHTML;
-
-    element.innerHTML = 'Preparing download <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>';
-    element.disabled = true;
-
+function downloadIso(version) {
     fetch("/downloads/" + version, {
         method: "GET"
     })
-    .then(response => response.blob())
-    .then(data => {
-        const a = document.createElement("a");
-        a.href = window.URL.createObjectURL(data);
-        a.download = "obision-" + version + ".iso";
-        a.click();
-    })
-    .finally(() => {
-        element.disabled = false;
-        element.innerHTML = innerHtml;
-    });
+
+    const a = document.createElement("a");
+    a.href = 'obision-' + version + '.iso'
+    a.download = "obision-" + version + ".iso";
+    a.click();
 }
