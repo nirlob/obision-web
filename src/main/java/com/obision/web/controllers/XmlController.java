29eb2a86ddecb7442ce51d064d9ed9a258afdc17
Jose Francisco Gonzalez <jfgs1609@gmail.com>
2023-12-01 18:47:01 +0100

    Commit

---
+++ src/main/java/com/obision/web/controllers/XmlController.java
@@ -0,0 +1,31 @@
+package com.obision.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/", produces = "application/xml")
+public class XmlController {
+    @GetMapping("/sitemap.xml")
+    public ResponseEntity<String> sitemap() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        stringBuilder.append("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+        stringBuilder.append("  <url>");
+        stringBuilder.append("    <loc>https://www.obision.com</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/contact</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/credits</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/documentation</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/install</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/releases</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/screenshots</loc>");
+        stringBuilder.append("    <loc>https://www.obision.com/timeverse</loc>");
+        stringBuilder.append("  </url>");
+        stringBuilder.append("</urlset>");
+
+        return ResponseEntity.ok(stringBuilder.toString());
+    }
+}
